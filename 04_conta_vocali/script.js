6f3a1fa442117e69592b48d032d12ab6e5ea345a
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-10 15:44:15 +0100

    complete exercise 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,9 +2,29 @@
 
 const word = 'javascript';
 
+let  letter_vocali = [];
+
 
 // Dichiara la funzione qui.
 
+function vocale(word){
+
+    let vocale = ["a","e","i","o","u"]
+
+    for(let i = 0; i < word.length; i++){
+
+        for(let y = 0; y < vocale.length; y++)
+
+        if(word[i] === vocale[y]){
+
+            letter_vocali.push(vocale[y])
+        }
+        
+    }
+    
+}
+vocale(word)
+console.log(letter_vocali);
 
 // Invoca la funzione qui e stampa il risultato in console
 
