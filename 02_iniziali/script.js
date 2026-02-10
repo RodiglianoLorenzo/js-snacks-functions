9f7b137ca0312762067e2dc700798ef22b2130c4
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-10 14:44:03 +0100

    complete second exercise

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,34 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+/* function first_letter_of_names (names) {
+    const letter_name = []
+    for(let i = 0; i < names.length; i++){
+        //const char = names[i] 
+        let element = names[i]
+        letter_name.push(element.charAt(0))
+        console.log("letter is:", letter_name[i]);
+        
+    }
+
+}
+first_letter_of_names(names)
+     */
+        
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const first_letter_of_names = names => {
+    const letter_name = []
+    for(let i = 0; i < names.length; i++){
+        //const char = names[i] 
+        let element = names[i]
+        letter_name.push(element.charAt(0))
+        console.log("letter is:", letter_name[i]);
+        
+    }
 
-
+}
+first_letter_of_names(names)
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
